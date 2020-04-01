cbe634d0a9f6065b72955590d26da0ee68b6fcb4
andywar65 <andy.war1965@gmail.com>
2020-04-02 00:42:49 +0200

    added invoice admin

--- admin.py
+++ admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Invoice
 
-# Register your models here.
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('number', 'client', 'active', 'date', 'amount', 'paid', )
