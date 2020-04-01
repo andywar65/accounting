618ed728429d9b4520733fe39e8e43dbbaf7aa10
andywar65 <andy.war1965@gmail.com>
2020-04-02 00:37:10 +0200

    added invoice model

---
+++ choices.py
@@ -0,0 +1,5 @@
+CAT = [('A01PR', 'A-Progettazione'),
+    ('A02DL', 'A-Direzione lavori'),
+    ('A03CT', 'A-Catasto'),
+    ('X', 'Altro'),
+    ]
--- models.py
+++ models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.timezone import now
+from .choices import *
 
-# Create your models here.
+class Invoice(models.Model):
+    number = models.CharField('Numero', max_length = 50, )
+    client = models.CharField('Cliente/Fornitore', max_length = 100, )
+    active = models.BooleanField('Attiva/Passiva', default=False, )
+    date = models.DateTimeField('Data', default = now, )
+    descr = models.TextField('Causale', null=True, blank=True, )
+    amount = models.FloatField('Imponibile', )
+    security = models.FloatField('Contributo previdenziale', null=True,
+        blank=True, )
+    vat = models.FloatField('IVA', null=True, blank=True, )
+    category = models.CharField('Categoria', max_length = 5, choices = CAT,
+        default = 'X', help_text = """'A' per le attive e 'P' per le passive.
+            """)
+    paid = models.BooleanField('Pagata', default=False, )
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        verbose_name = 'Fattura'
+        verbose_name_plural = 'Fatture'
+        ordering = ('-date', )
