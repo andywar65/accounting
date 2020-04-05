f0fe6a65571772a3f7209dfc0988641e1e2c7c1e
andywar65 <andy.war1965@gmail.com>
2020-04-05 23:49:36 +0200

    added get total method in invoice model

--- models.py
+++ models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from django.utils.timezone import now
-from .choices import *
+from .choices import CAT
 
 class Invoice(models.Model):
     number = models.CharField('Numero', max_length = 50, )
     client = models.CharField('Cliente/Fornitore', max_length = 100, )
-    active = models.BooleanField('Attiva/Passiva', default=False, )
+    active = models.BooleanField('Attiva', default=False,
+        help_text = """Se selezionato è attiva, altrimenti è passiva""")
     date = models.DateTimeField('Data', default = now, )
     descr = models.TextField('Causale', null=True, blank=True, )
     amount = models.DecimalField('Imponibile', max_digits=8, decimal_places=2)
-    security = models.DecimalField('Contributo previdenziale', null=True,
-        blank=True, max_digits=8, decimal_places=2 )
-    vat = models.DecimalField('IVA', null=True, blank=True, max_digits=8,
+    security = models.DecimalField('Contributo previdenziale', default = 0.00,
+        max_digits=8, decimal_places=2 )
+    vat = models.DecimalField('IVA', default = 0.00, max_digits=8,
         decimal_places=2 )
     category = models.CharField('Categoria', max_length = 5, choices = CAT,
         default = 'X', help_text = """'A' per le attive e 'P' per le passive.
@@ -20,6 +21,9 @@ class Invoice(models.Model):
 
     def __str__(self):
         return self.number
+
+    def get_total(self):
+        return self.amount + self.security + self.vat
 
     class Meta:
         verbose_name = 'Fattura'
