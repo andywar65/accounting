50a361eb6548df401a2979a1c5c26d4e077cdef7
andywar65 <andy.war1965@gmail.com>
2020-10-01 23:41:19 +0200

    accounting group is created upon migration

--- apps.py
+++ apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+def create_accounting_group(sender, **kwargs):
+    from django.contrib.auth.models import Permission, Group
+    grp, created = Group.objects.get_or_create(name='Accounting')
+    if created:
+        permission = Permission.objects.filter(codename__in=("view_invoice",
+            'add_invoice', 'change_invoice', 'delete_invoice', 'add_csvinvoice'))
+        grp.permissions.set(permission)
 
 class AccountingConfig(AppConfig):
     name = 'accounting'
+
+    def ready(self):
+        post_migrate.connect(create_accounting_group, sender=self)
