23833315be30b7285a5d0fc2ab3ecbe87ad53e6d
andywar65 <andy.war1965@gmail.com>
2020-10-07 17:47:40 +0200

    first test for invoice model

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from accounting.models import Invoice, CSVInvoice
+
+class InvoiceModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        invoice = Invoice.objects.create(number='001', client = 'Mr. Client',
+            active = True, date = '2020-05-09', descr = 'My first invoice',
+            amount = 1000, security = 10, vat = 100, category = 'A00',
+            paid = False)
+
+    def test_invoice_str_method(self):
+        inv = Invoice.objects.get(number='001')
+        self.assertEquals(inv.__str__(), '001')
