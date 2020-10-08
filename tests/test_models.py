0a3c2cca12d088a1a12dd526c93f961a5a1a246a
andywar65 <andy.war1965@gmail.com>
2020-10-08 19:16:01 +0200

    added three simple model tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,7 +9,21 @@ class InvoiceModelTest(TestCase):
             active = True, date = '2020-05-09', descr = 'My first invoice',
             amount = 1000, security = 10, vat = 100, category = 'A00',
             paid = False)
+        csvinvoice = CSVInvoice.objects.create(date = '2020-05-09 15:53:00+02',
+            csv = 'accounting/sample.csv')
 
     def test_invoice_str_method(self):
         inv = Invoice.objects.get(number='001')
         self.assertEquals(inv.__str__(), '001')
+
+    def test_invoice_get_total_method(self):
+        inv = Invoice.objects.get(number='001')
+        self.assertEquals(inv.get_total(), 1110)
+
+    def test_csvinvoice_str_method(self):
+        csvinv = CSVInvoice.objects.get(date='2020-05-09 15:53:00+02')
+        self.assertEquals(csvinv.__str__(), f'File - {csvinv.id}')
+
+    def test_csvinvoice_get_filename(self):
+        csvinv = CSVInvoice.objects.get(date='2020-05-09 15:53:00+02')
+        self.assertEquals(csvinv.get_filename(), 'sample.csv')
