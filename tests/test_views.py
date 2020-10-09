0d1747f30dbe80743c099b6e29ba2cc93b74b5e3
andywar65 <andy.war1965@gmail.com>
2020-10-09 18:57:44 +0200

    added context tests for invoice archive view

--- tests/test_views.py
+++ tests/test_views.py
@@ -17,6 +17,14 @@ class InvoiceViewTest(TestCase):
             content_type=content_type,
         )
         viewer.user_permissions.add(permission)
+        invoice2 = Invoice.objects.create(number='002', client = 'Mr. Client',
+            active = True, date = '2020-05-02', descr = 'My first invoice',
+            amount = 1000, security = 10, vat = 100, category = 'A00',
+            paid = False)
+        invoice3 = Invoice.objects.create(number='003', client = 'Mr. Client',
+            active = False, date = '2020-05-03', descr = 'My first invoice',
+            amount = 1000, security = 10, vat = 100, category = 'P00',
+            paid = False)
 
     def test_invoice_archive_view_status_code_not_logged(self):
         response = self.client.get(reverse('invoices:index'))
@@ -37,3 +45,29 @@ class InvoiceViewTest(TestCase):
             'password':'P4s5W0r6'})
         response = self.client.get(reverse('invoices:index'))
         self.assertTemplateUsed(response, 'accounting/invoice_archive.html')
+
+    def test_invoice_archive_view_context(self):
+        self.client.post('/accounts/login/', {'username':'viewer',
+            'password':'P4s5W0r6'})
+        all_invoices = Invoice.objects.all()
+        response = self.client.get(reverse('invoices:index'))
+        self.assertQuerysetEqual(response.context['all_invoices'], all_invoices,
+            transform=lambda x: x)
+
+    def test_invoice_archive_view_context_alternatives(self):
+        self.client.post('/accounts/login/', {'username':'viewer',
+            'password':'P4s5W0r6'})
+        response = self.client.get('/fatture/?created=001')
+        self.assertEqual(response.context['created'], '001')
+        response = self.client.get('/fatture/?modified=001')
+        self.assertEqual(response.context['modified'], '001')
+        response = self.client.get('/fatture/?deleted=001')
+        self.assertEqual(response.context['deleted'], '001')
+        response = self.client.get('/fatture/?csv_created=99&csv_modified=18&csv_failed=1')
+        self.assertEqual(response.context['csv_created'], '99')
+        self.assertEqual(response.context['csv_modified'], '18')
+        self.assertEqual(response.context['csv_failed'], '1')
+
+    def test_invoice_year_view_status_code_not_logged(self):
+        response = self.client.get(reverse('invoices:year', kwargs={'year': '2020'}))
+        self.assertEqual(response.status_code, 302)
