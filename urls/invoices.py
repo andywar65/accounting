35ea85ca3a71f3881cf6f059ab74759264d92045
andywar65 <andy.war1965@gmail.com>
2020-11-18 19:34:58 +0100

    i18n fo urlpatterns

--- urls/invoices.py
+++ urls/invoices.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
+
 from accounting.views import (InvoiceArchiveIndexView, InvoiceYearArchiveView,
     InvoiceMonthArchiveView, InvoiceCreateView, InvoiceUpdateView,
     InvoiceDeleteView, CSVInvoiceCreateView, year_download, month_download,
@@ -9,14 +11,16 @@ urlpatterns = [
     path('', InvoiceArchiveIndexView.as_view(), name = 'index'),
     path('<int:year>/', InvoiceYearArchiveView.as_view(),
         name = 'year'),
-    path('<int:year>/download/', year_download, name = 'year_download'),
+    path(_('<int:year>/download/'), year_download, name = 'year_download'),
     path('<int:year>/<int:month>/', InvoiceMonthArchiveView.as_view(),
         name = 'month'),
-    path('<int:year>/<int:month>/download/', month_download,
+    path(_('<int:year>/<int:month>/download/'), month_download,
         name = 'month_download'),
-    path('add/', InvoiceCreateView.as_view(), name = 'add'),
-    path('add/csv/', CSVInvoiceCreateView.as_view(), name = 'csv'),
-    path('change/invoice/<pk>/', InvoiceUpdateView.as_view(), name = 'change'),
-    path('delete/invoice/<pk>/', InvoiceDeleteView.as_view(), name = 'delete'),
+    path(_('add/'), InvoiceCreateView.as_view(), name = 'add'),
+    path(_('add/csv/'), CSVInvoiceCreateView.as_view(), name = 'csv'),
+    path(_('change/invoice/<pk>/'), InvoiceUpdateView.as_view(),
+        name = 'change'),
+    path(_('delete/invoice/<pk>/'), InvoiceDeleteView.as_view(),
+        name = 'delete'),
     #path('email/', CSVInvoiceMailTemplateView.as_view(), name = 'email'),
     ]
